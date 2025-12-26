0b39887134b5afcaa52407f6cdec2b8d9c674795
George Psarakis <giwrgos.psarakis@gmail.com>
2025-12-26 08:19:16 +0200

    Add Recoverer example with errgroup & zap

--- examples/zap_test.go
+++ examples/zap_test.go
@@ -2,9 +2,12 @@ package examples
 
 import (
 	"errors"
+	"fmt"
 
 	"go.uber.org/zap"
+	"golang.org/x/sync/errgroup"
 
+	"github.com/georgepsarakis/errorcontext"
 	zaperrorcontext "github.com/georgepsarakis/errorcontext/backend/zap"
 )
 
@@ -34,4 +37,38 @@ func tryWithError() error {
 		zap.String("path", "/a/b"),
 		zap.Bool("enabled", true),
 	)
+}
+
+func ExampleRecoverer() {
+	cfg := zap.NewProductionConfig()
+	zapLogger, err := cfg.Build()
+	if err != nil {
+		panic(err)
+	}
+	grp := errgroup.Group{}
+	recoverer := errorcontext.Recoverer[*zaperrorcontext.Error]{
+		NewErrorFunc: func(p errorcontext.Panic) *zaperrorcontext.Error {
+			return zaperrorcontext.FromPanic(p)
+		},
+	}
+	grp.Go(recoverer.WrapFunc(func() error {
+		panic("something bad happened")
+	}))
+
+	grp.Go(func() error {
+		fmt.Println("Hello World")
+		return nil
+	})
+	if err := grp.Wait(); err != nil {
+		var ze *zaperrorcontext.Error
+		var isPanic bool
+		if errors.As(err, &ze) {
+			isPanic = ze.IsPanic()
+		}
+		zapLogger.Warn("something failed",
+			zap.Dict("error_context", zaperrorcontext.AsContext(err)...),
+			zap.Bool("is_panic", isPanic),
+			zap.Error(err))
+	}
+	//Output: Hello World
 }
